6c40e61e34e93e7ce2f7d6b125d866f77c2f8dec
Jeniffer Hurtado Coronado <62664863+Jennimar17@users.noreply.github.com>
2022-02-26 19:29:24 -0300

    style(home): add assets & style to component home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 export default function Home() {
-  return <div>HOME</div>;
+  return (
+    <div className="card">
+      <div className="card-img--logo">
+        <img src="/assets/zignaly_logo.svg" alt="" className="logo" />
+      </div>
+      <div className="card-img--subtitle">
+        <img src="/assets/PhoneShop.png" alt="phone logo" />
+      </div>
+      <Link to="/phones">
+        <button className="card-button">Entrar</button>
+      </Link>
+    </div>
+  );
 }
